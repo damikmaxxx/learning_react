98158d4647630034cc3f57608c756df4d4669880
damikmaxxx <damik200355555@mail.ru>
2021-04-06 20:52:50 +0300

    53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,24 +1,32 @@
 import User from './User/User';
 import * as axios from "axios";
 import userPhoto from '../../assets/images/man-300x300.png'
+import React from 'react'
+class Users extends React.Component {
 
-export const Users = (props) => {
-   
-    if (props.users.length === 0){
+    constructor(props){
+        super(props)
+        this.getUsers()
+    }
+
+    getUsers = () => {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
+            this.props.setUsers(response.data.items)
         })
     }
-    
-    let users = props.users.map(u => <User toggleFollow={props.toggleFollow} idUser={u.id} followed={u.followed} photoUrl={userPhoto} name={u.name} status={u.status} />)
-    return (
-        <div>
+
+    render() {
+        return (
             <div>
-                Users
+                <button onClick={this.getUsers}>get Users</button>
+                <div>
+                    Users
+                </div>
+                {this.props.users.map(u => <User toggleFollow={this.props.toggleFollow} idUser={u.id} followed={u.followed} photoUrl={userPhoto} name={u.name} status={u.status} />)}
             </div>
-            {users}
-        </div>
-    )
+        )
+
+    }
 }
 
-export default Users;
+export default Users
